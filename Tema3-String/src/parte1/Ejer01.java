950647149f4405ab6560e87de265c926e3a60785
anamontero14 <ana.montero@iesnervion.es>
2024-12-10 13:48:03 +0100

    Cosas

---
+++ Tema3-String/src/parte1/Ejer01.java
@@ -0,0 +1,28 @@
+package parte1;
+
+import java.util.Scanner;
+
+public class Ejer01 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Introducir por teclado dos frases e indicar cuál de ellas es la más corta, es
+		 * decir, la que contiene menos caracteres.
+		 */
+		
+		//creacion del scanner
+		Scanner leer = new Scanner(System.in);
+		
+		//variable para almacenar la primera cadena
+		String cadena1;
+		
+		//variable para almacenar la segunda cadena
+		
+		
+		//cerrar scanner
+		leer.close();
+
+	}
+
+}
